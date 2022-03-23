318647d859985b9e68c42aef1ed143867790d180
megosha <mvgosha@gmail.com>
2022-03-23 13:07:37 +0700

    small fix

--- unitedhelpbot/settings.py
+++ unitedhelpbot/settings.py
@@ -27,7 +27,7 @@ env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env('DJANGO_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', False)
